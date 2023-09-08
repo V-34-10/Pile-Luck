c475c682ca599a0ec8e26d40809e105e1471a63c
Vlad <vladandryschenko98@gmail.com>
2023-09-08 18:04:02 +0300

    MenuActivity: add checkUserSignIn(), controlButton()

--- app/src/main/java/com/pyramidal/luuck/ui/main/menu/MenuActivity.kt
+++ app/src/main/java/com/pyramidal/luuck/ui/main/menu/MenuActivity.kt
@@ -1,6 +1,8 @@
 package com.pyramidal.luuck.ui.main.menu
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
@@ -8,17 +10,23 @@ import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import com.pyramidal.luuck.R
 import com.pyramidal.luuck.databinding.ActivityMenuBinding
+import com.pyramidal.luuck.ui.login.LoginActivity
+import com.pyramidal.luuck.ui.main.privacy.PrivacyActivity
 import com.pyramidal.luuck.ui.main.scene.SceneActivity
+import com.pyramidal.luuck.ui.main.settings.SettingsActivity
 import com.pyramidal.luuck.utils.HideUIConfigUtils
 
 class MenuActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMenuBinding.inflate(layoutInflater) }
+    private lateinit var sharedPref: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view = binding.root
         setContentView(view)
         HideUIConfigUtils.hideUINavigation(this)
         initRecycler()
+        controlButton()
+        checkUserSignIn()
     }
 
     private fun controlButton() {
@@ -26,16 +34,22 @@ class MenuActivity : AppCompatActivity() {
         val animation = AnimationUtils.loadAnimation(this, R.anim.scale_up)
         binding.btnExit.setOnClickListener {
             binding.btnExit.startAnimation(animation)
-            //loadingNextActivity()
+            finish()
         }
 
         binding.btnRegister.setOnClickListener {
             binding.btnRegister.startAnimation(animation)
-            //loadingNextActivity()
+            // run LoginActivity
+            val go = Intent(this@MenuActivity, LoginActivity::class.java)
+            startActivity(go)
+            finish()
         }
         binding.btnSettings.setOnClickListener {
             binding.btnSettings.startAnimation(animation)
-
+            // run LoginActivity
+            val go = Intent(this@MenuActivity, SettingsActivity::class.java)
+            startActivity(go)
+            finish()
         }
     }
 
@@ -74,6 +88,17 @@ class MenuActivity : AppCompatActivity() {
         binding.gamesContainer.adapter = adapter
         binding.gamesContainer.layoutManager = layoutManager
         layoutManager.spanSizeLookup = SpanSizeLookup(adapter)
+    }
+
+    private fun checkUserSignIn() {
+        sharedPref = getSharedPreferences("my_prefs", MODE_PRIVATE)
+        val hasEmail = sharedPref?.contains("email_key") ?: false
+        val hasPhone = sharedPref?.contains("phone_key") ?: false
+        if (hasEmail || hasPhone) {
+            // change image on statusLevel
+            binding.btnRegister.setImageResource(R.drawable.status_level)
+            binding.btnRegister.isClickable = false
+        }
     }
 
     @Deprecated("Deprecated in Java")
