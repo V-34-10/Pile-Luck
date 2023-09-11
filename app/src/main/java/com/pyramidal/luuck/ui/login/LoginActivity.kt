591e75ed31e008f484229cc1f7d5a78e1ec91d97
Vlad <vladandryschenko98@gmail.com>
2023-09-11 22:22:11 +0300

    fix LoginActivity: hideNavigation

--- app/src/main/java/com/pyramidal/luuck/ui/login/LoginActivity.kt
+++ app/src/main/java/com/pyramidal/luuck/ui/login/LoginActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.pyramidal.luuck.R
+import com.pyramidal.luuck.utils.HideUIConfigUtils
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        HideUIConfigUtils.hideUINavigation(this)
     }
 
     @Deprecated("Deprecated in Java")
