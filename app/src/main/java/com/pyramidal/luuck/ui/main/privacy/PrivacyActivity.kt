4c1480d5e3cf4c79b669c27264a1ded055f0d8b2
Vlad <vladandryschenko98@gmail.com>
2023-10-03 16:10:10 +0300

    PrivacyActivity: fix show WebViewPrivacy

--- app/src/main/java/com/pyramidal/luuck/ui/main/privacy/PrivacyActivity.kt
+++ app/src/main/java/com/pyramidal/luuck/ui/main/privacy/PrivacyActivity.kt
@@ -1,16 +1,24 @@
 package com.pyramidal.luuck.ui.main.privacy
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.View.VISIBLE
 import android.view.animation.AnimationUtils
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.pyramidal.luuck.R
 import com.pyramidal.luuck.databinding.ActivityPrivacyBinding
 import com.pyramidal.luuck.ui.login.LoginActivity
 import com.pyramidal.luuck.ui.utils.HideUIConfigUtils
 
 class PrivacyActivity : AppCompatActivity() {
+    private lateinit var WebViewShow: WebView
+    private var mainLink: String = "https://www.google.com/"
     private val binding by lazy { ActivityPrivacyBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +32,8 @@ class PrivacyActivity : AppCompatActivity() {
         val animation = AnimationUtils.loadAnimation(this, R.anim.scale_up)
         binding.btnPrivacy.setOnClickListener {
             binding.btnPrivacy.startAnimation(animation)
-            RemotePrivacy.openPrivacyLink(this)
+            binding.WebViewShow.visibility = VISIBLE
+            initWebView()
         }
         binding.btnYes.setOnClickListener {
             binding.btnYes.startAnimation(animation)
@@ -34,7 +43,35 @@ class PrivacyActivity : AppCompatActivity() {
             binding.btnNo.startAnimation(animation)
             loadingLoginActivity()
         }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebView() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        WebViewShow = findViewById(R.id.WebViewShow)
+        WebViewShow.webViewClient = WebViewClient()
+        WebViewShow.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            allowContentAccess = true
+        }
+        WebViewShow.webViewClient = WebViewClient()
+        WebViewShow.loadUrl(mainLink)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (event.action == KeyEvent.ACTION_DOWN) {
+            when (keyCode) {
+                KeyEvent.KEYCODE_BACK -> {
+                    if (WebViewShow.canGoBack()) {
+                        WebViewShow.goBack()
+                        return true
+                    }
+                }
+            }
+        }
+        return super.onKeyDown(keyCode, event)
     }
 
     private fun loadingLoginActivity() {
